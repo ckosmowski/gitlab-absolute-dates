737d6ceb3b4edb72831b69ae3d77757a0b14e74d
Chris <ksmwsk@gmail.com>
2018-08-08 10:14:50 +0200

    Update absoluteDates.js

--- absoluteDates.js
+++ absoluteDates.js
@@ -1,6 +1,33 @@
-setTimeout(function(){
+function applyGitlabAbsoluteDates() {
     let nodes = document.querySelectorAll("time");
     nodes.forEach(function(node){
-      node.style.backgroundColor = 'red';
+      var datetime = node.getAttribute('datetime');
+      var date = new Date(datetime);
+      var newDateString = date.toLocaleDateString("de-DE", {day: '2-digit', month: '2-digit', year: 'numeric', minute:'2-digit', hour: '2-digit'});
+	  node.textContent = newDateString + " Uhr";
     });
-  }, 1000);
+}
+
+setTimeout(function(){
+    applyGitlabAbsoluteDates();
+}, 1000);
+
+// select the target node
+var target = document.querySelector('.content-wrapper');
+ 
+// create an observer instance
+var observer = new MutationObserver(function(mutations) {
+    var isChildList = false;
+    mutations.forEach(function(mutation) {
+        isChildList = isChildList || (mutation.type == "childList")
+    });
+    if (isChildList) {
+        applyGitlabAbsoluteDates();
+    }
+});
+ 
+// configuration of the observer:
+var config = { attributes: true, childList: true, characterData: true, subtree: true}
+ 
+// pass in the target node, as well as the observer options
+observer.observe(target, config);
